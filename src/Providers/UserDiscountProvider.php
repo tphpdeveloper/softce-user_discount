ce42ac1980ea481c09bd3567c6c77674219a5901
Igor <ig@c-ua.com>
2018-06-11 16:45:15 +0300

    Created migration file

---
+++ src/Providers/UserDiscountProvider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UserCE
+ * Date: 11.06.2018
+ * Time: 14:46
+ */
+
+namespace Softce\UserDiscount\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class UserDiscountProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(dirname(__DIR__) . '\routes\web.php');
+        $this->loadViewsFrom(dirname(__DIR__) . '\views', 'user_discount');
+        $this->loadMigrationsFrom(dirname(__DIR__) . '/migrations');
+
+        $slider = DB::table('admin_menus')->where('name', 'Скидки пользователей')->first();
+        if (is_null($slider)) {
+            DB::table('admin_menus')->insert([
+                'admin_menu_id' => 5,
+                'name' => 'Скидки пользователей',
+                'icon' => 'fa-percent',
+                'route' => 'admin.user_discount.edit',
+                'o' => 0
+            ]);
+        }
+
+    }
+
+    public function register()
+    {
+        $this->app->singleton(TypeButton::class, function(){
+            return new BuildButton(Type::with('product_type')->get());
+        });
+    }
+
+}
---
+++ src/migrations/2018_06_11_163706_create_user_category_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('categoty_id')->nullable();
+            $table->foreign('categoty_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->float('discount', 4, 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_category');
+    }
+}
