2f3f78557adbffc0bdeb90a6a4257301309e46a7
Igor <ig@c-ua.com>
2018-07-03 14:29:53 +0300

    Some changes

--- src/Providers/UserDiscountServiceProvider.php
+++ src/Providers/UserDiscountServiceProvider.php
@@ -16,8 +16,8 @@ class UserDiscountServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(dirname(__DIR__) . '\routes\web.php');
-        $this->loadViewsFrom(dirname(__DIR__) . '\views', 'user_discount');
+        $this->loadRoutesFrom(dirname(__DIR__) . '/routes/web.php');
+        $this->loadViewsFrom(dirname(__DIR__) . '/views', 'user_discount');
         $this->loadMigrationsFrom(dirname(__DIR__) . '/migrations');
     }
 
